87d4695ef5db23d5d744097586947c234966e454
Emi Yokoo Mantilla <emimantilla@Christians-Air.lan1>
2021-01-11 17:00:39 -0600

    completed RPS function

--- main.js
+++ main.js
@@ -11,13 +11,45 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+/*the function returns the following
+*if hand1 is the winning hand return "hand one wins!"
+*if hand2 is the winning hand return "hand two wins!"
+*if it is a tie return "it's a tie!"
+*/
+
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
 
   // Write code here
   // Use the unit test to see what is expected
 
+  // we set a modification here because the program is case sensitive and you want to satisfy the if statements
+hand1 = hand1.trim().toLowerCase();
+hand2 = hand2.trim().toLowerCase(); 
+
+  if (hand1 === hand2) {
+    return "It's a tie!"
+  } 
+  if (hand1 === "rock" && hand2 === "paper"){
+      return "Hand two wins!"
+  }
+  if (hand1 === "rock" && hand2 === "scissors"){
+    return "Hand one wins!"
+  }  
+  if (hand1 === "paper" && hand2 === "scissors"){
+  return "Hand two wins!"
+  }
+  if (hand1 === "paper" && hand2 === "rock"){
+    return "Hand one wins!"
+  }
+  if (hand1 === "scissors" && hand2 === "rock"){
+    return "Hand two wins!"
+  }
+  if (hand1 === "scissors" && hand2 === "paper"){
+    return "Hand one wins!"
+  }
 }
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
@@ -25,7 +57,7 @@ const rockPaperScissors = (hand1, hand2) => {
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+      console.log(rockPaperScissors(answer1, answer2));
       getPrompt();
     });
   });
@@ -58,5 +90,20 @@ if (typeof describe === 'function') {
 
   // always returns ask the user for another input
   getPrompt();
-
 }
+
+
+/* Code Plan:
+1. User1 input of rock, paper, or scissors.
+2. User2 input of rock, paper, or scissors.
+3. Compare User1 input to User2 input.
+4. If User1 input is 'rock' and User2 input is 'scissor', User1 wins.
+5. If User1 input is 'rock' and User2 input is 'paper', User2 wins.
+6. If User1 input is 'rock' and User2 input is 'rock', it's a tie.
+7. If User1 input is 'paper' and User2 input is 'rock', User1 wins.
+8. If User1 input is 'paper' and User2 input is 'scissors', User2 wins.
+9. If User1 input is 'paper' and User2 input is 'paper', it's a tie.
+10. If User1 input is 'scissors' and User2 input is 'paper', User1 wins.
+11. If User1 input is 'scissors' and User2 input is 'rock', User2 wins.
+12. If User1 input is 'scissors' and User2 input is 'scissors', it's a tie.
+*/
